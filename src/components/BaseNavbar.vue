41491a3f86836c7133704a0413ad47a91931d0e5
Lukáš Podvojský <lucas.podvojsky@gmail.com>
2023-12-11 14:14:00 +0100

    Add testing project structure

---
+++ src/components/BaseNavbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-amber-400">Hello this is navbar {{ count }}</div>
+  <button @click="count++">Increment</button>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const count = ref(0);
+</script>
+
+<style scoped></style>
